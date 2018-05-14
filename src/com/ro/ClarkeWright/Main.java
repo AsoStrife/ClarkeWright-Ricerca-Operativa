24582393ec831d1201af7707a15545ed4cf508a1
Andrea <cloud_aso@hotmail.it>
2018-05-14 17:09:58 +0200

    First Commit

---
+++ src/com/ro/ClarkeWright/Main.java
@@ -0,0 +1,8 @@
+package com.ro.ClarkeWright;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Progetto Ricerca Operativa. Andrea Corriga & Riccardo Mulas");
+    }
+}
