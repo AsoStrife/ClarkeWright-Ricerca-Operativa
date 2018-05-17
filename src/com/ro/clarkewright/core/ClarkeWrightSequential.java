c8aa4ae4f238799c5b92150ffcefae4066adc5db
Andrea Corriga <cloud_aso@hotmail.it>
2018-05-17 15:17:27 +0200

    Sort main routes

--- src/com/ro/clarkewright/core/ClarkeWrightSequential.java
+++ src/com/ro/clarkewright/core/ClarkeWrightSequential.java
@@ -4,6 +4,8 @@ import com.ro.clarkewright.model.DistanceMatrix;
 import com.ro.clarkewright.model.Route;
 import com.ro.clarkewright.model.SavingsMatrix;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class ClarkeWrightSequential {
     // Contain the .vrp file istance
@@ -12,7 +14,7 @@ public class ClarkeWrightSequential {
     // This object contain the distance matrix
     private DistanceMatrix distanceMatrix;
 
-    // This ojbect contain the saving matrix
+    // This object contain the saving matrix
     private SavingsMatrix savingsMatrix;
 
     // Contain the main routes
@@ -23,6 +25,7 @@ public class ClarkeWrightSequential {
         this.distanceMatrix = new DistanceMatrix(instance.getDepot(), instance.getNodes());
         this.savingsMatrix = new SavingsMatrix(instance.getDepot(), instance.getNodes(), distanceMatrix);
 
+        // generate the main routes
         mainRoutesHandler();
 
         debug();
@@ -35,13 +38,35 @@ public class ClarkeWrightSequential {
         for(int i = 0; i < instance.nodeSize(); i++){
             mainRoutes.add( new Route(instance.getDepot(), instance.getNode(i), distanceMatrix));
         }
+        // sort
+        orderRoutes();
     }
 
+    /**
+     * Sort the main routes decreasing
+     */
+    public void orderRoutes(){
+        Collections.sort(mainRoutes, new Comparator<Route>() {
+            @Override
+            public int compare(Route c1, Route c2) {
+                return Double.compare(c1.getTotalDistance(), c2.getTotalDistance());
+            }
+        });
+    }
+
+    /**
+     * DEBUG METHOD
+     */
     private void debug(){
         System.out.println("-- DISTANCE MATRIX --");
         distanceMatrix.print();
 
         System.out.println("-- SAVINGS MATRIX --");
         savingsMatrix.print();
+
+        System.out.println("-- ROUTE DISTANCES --");
+        for (int i = 0; i < mainRoutes.size(); i++) {
+            System.out.println(mainRoutes.get(i).getTotalDistance());
+        }
     }
 }
